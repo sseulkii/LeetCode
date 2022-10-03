249f62a1a76ead59b9613e0ae8282f33d9cce38f
Seulki Lee <89380341+sseulkii@users.noreply.github.com>
2022-10-03 23:04:53 +0900

    Time: 29 ms (97.19%), Space: 13.8 MB (56.87%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.py
+++ 226-invert-binary-tree/226-invert-binary-tree.py
@@ -10,25 +10,32 @@ class Solution:
         if not root:
             return None
         
-        def invert(node):
-            if not node.left and not node.right:
-                return node
-            
-            if not node.left:
-                node.left = invert(node.right)
-                node.right = None
-                return node
-            
-            if not node.right:
-                node.right = invert(node.left)
-                node.left = None
-                return node
-            
-            left = invert(node.left)
-            right = invert(node.right)
-                
-            node.left, node.right = right, left
-            
-            return node
+        root.left, root.right = root.right, root.left
         
-        return invert(root)
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        
+        return root
+        
+#         def invert(node):
+#             if not node.left and not node.right:
+#                 return node
+            
+#             if not node.left:
+#                 node.left = invert(node.right)
+#                 node.right = None
+#                 return node
+            
+#             if not node.right:
+#                 node.right = invert(node.left)
+#                 node.left = None
+#                 return node
+            
+#             left = invert(node.left)
+#             right = invert(node.right)
+                
+#             node.left, node.right = right, left
+            
+#             return node
+        
+#         return invert(root)
