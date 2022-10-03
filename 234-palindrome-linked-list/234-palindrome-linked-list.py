62f20a4aa54a784333a5fcccc6055d71232b3f7d
Seulki Lee <89380341+sseulkii@users.noreply.github.com>
2022-10-03 22:31:02 +0900

    Time: 1188 ms (62.37%), Space: 46.5 MB (62.10%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -7,8 +7,10 @@ class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
         arr = []
         
-        while head:
-            arr.append(head.val)
-            head = head.next
+        now = head
+        
+        while now:
+            arr.append(now.val)
+            now = now.next
             
         return arr == arr[::-1]
