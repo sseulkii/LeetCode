eac63f58fa0492b4f3cba72e5c2ad2a668b1bbfd
Seulki Lee <89380341+sseulkii@users.noreply.github.com>
2022-10-03 17:54:03 +0900

    Time: 47 ms (60.06%), Space: 13.8 MB (56.94%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,14 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        d = [0] * (n + 1)
+        d = [0] * 46
         
-        if n == 1:
-            return 1
-        
-        d[1] = 1
-        d[2] = 2
-        
-        for i in range(3, n + 1):
-            d[i] = d[i - 1] + d[i - 2]
+        def climb(x):
             
-        return d[n]
+            if x == 1 or x == 2:
+                return x
+            
+            if d[x] != 0:
+                return d[x]
+            
+            d[x] = climb(x - 1) + climb(x - 2)
+            
+            return d[x]
+            
+        return climb(n)
