df2b741c2ca77cb4c52d888ef41e754fc0cbc377
Seulki Lee <89380341+sseulkii@users.noreply.github.com>
2022-10-08 00:30:58 +0900

    Time: 324 ms (90.39%), Space: 16.5 MB (64.21%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,27 +1,21 @@
-from collections import deque
-
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        m = len(grid)
-        n = len(grid[0])
-        
-        def bfs(start_i, start_j):
-            q = deque([(start_i, start_j)])
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0  or j >= n or grid[i][j] == "0":
+                return
             
             dx = [-1, 1, 0, 0]
             dy = [0, 0, -1, 1]
             
-            while q:
-                i, j = q.popleft()
-                grid[i][j] = "0"
-                
-                for idx in range(4):
-                    nx, ny = i + dx[idx], j + dy[idx]
-                    
-                    if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == "1" and (nx, ny) not in q:
-                        q.append((nx, ny))
-                        
+            grid[i][j] = "0"
+            
+            for idx in range(4):
+                nx, ny = i + dx[idx], j + dy[idx]
+                dfs(nx, ny)
+            
+        m = len(grid)
+        n = len(grid[0])
         
         islands = 0
         
@@ -29,6 +23,6 @@ class Solution:
             for j in range(n):
                 if grid[i][j] == "1":
                     islands += 1
-                    bfs(i, j)
-                    
+                    dfs(i, j)
+        
         return islands
