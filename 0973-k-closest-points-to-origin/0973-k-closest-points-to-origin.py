9d02b396ec222e582a3e3660499b1ae9522d1117
Seulki Lee <89380341+sseulkii@users.noreply.github.com>
2022-10-13 10:57:28 +0900

    Time: 1303 ms (61.55%), Space: 19.9 MB (97.09%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -13,6 +13,7 @@ class Solution:
             heapq.heappush(h, (get_len(point), point))
             
         answer = []
+        
         for _ in range(k):
             answer.append(heapq.heappop(h)[1])
             
