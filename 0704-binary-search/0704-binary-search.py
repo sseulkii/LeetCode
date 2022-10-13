505ddc46de75a4e02d97f572f94237c7950a8edd
Seulki Lee <89380341+sseulkii@users.noreply.github.com>
2022-10-13 10:58:16 +0900

    Time: 741 ms (5.06%), Space: 15.4 MB (96.95%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,6 +3,7 @@ from bisect import bisect_left
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         idx = bisect_left(nums, target)
+        
         if idx < len(nums) and nums[idx] == target:
             return idx
         return -1
