c84d6b437a4daed785b906058703e89da29bad22
Seulki Lee <89380341+sseulkii@users.noreply.github.com>
2022-10-13 11:21:16 +0900

    Time: 47 ms (95.64%), Space: 14.2 MB (24.81%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -4,6 +4,7 @@ class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         nums1 = sorted(set(nums1))
         nums2 = sorted(set(nums2))
+        
         ret = []
         for n in nums1:
             idx = bisect_left(nums2, n)
