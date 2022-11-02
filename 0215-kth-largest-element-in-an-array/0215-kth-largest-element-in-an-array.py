0b0f5a831634a66be2f9541a4bff371f0fba3c6f
Seulki Lee <89380341+sseulkii@users.noreply.github.com>
2022-11-03 00:13:07 +0900

    Time: 1059 ms (70.67%), Space: 28.2 MB (32.09%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -6,16 +6,12 @@ class Solution:
         
         for num in nums:
             heapq.heappush(q, -num)
-            
-        for _ in range(k - 1):
-            heapq.heappop(q)
-            
-        return -heapq.heappop(q)
-        # result = []
-#         while q:
-#             result.append(-heapq.heappop(q))
-#             # if len(result) == k - 1:
-#             #     break
+
+        result = []
+        while q:
+            result.append(-heapq.heappop(q))
+            if len(result) == k:
+                break
                 
-#         return result[k - 1]
+        return result[k - 1]
             
